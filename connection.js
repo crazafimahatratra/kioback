6b7041d14c6e6d6c5ac6d950eb7ffc5d09cc54db
Christophe <rhisty@gmail.com>
2019-11-27 15:52:18 +0300

    logging false

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'data.db',
-    logging: true
+    logging: false
 });
 
 module.exports = sequelize
