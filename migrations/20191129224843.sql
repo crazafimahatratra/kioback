77d17466196d567a2c6d4b347d8c252464092f93
Christophe <rhisty@gmail.com>
2019-11-30 00:30:43 +0300

    Bill Item

--- migrations/20191129224843.sql
+++ migrations/20191129224843.sql
@@ -5,3 +5,13 @@ CREATE TABLE bill (
     customer VARCHAR (64) NOT NULL,
     address  VARCHAR (64) NOT NULL
 );
+
+CREATE TABLE bill_item (
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    bill_id      INTEGER REFERENCES bill (id) ON DELETE CASCADE
+                                              ON UPDATE CASCADE,
+    operation_id INTEGER REFERENCES operation (id) ON DELETE CASCADE
+                                                   ON UPDATE CASCADE,
+    qty          INTEGER NOT NULL,
+    price        INTEGER NOT NULL
+);
